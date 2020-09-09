12f0fc3d08522f7add49a2f83b10bd4f07fdb660
Leonel Sarmiento <leonel.sarmiento@outlook.com>
2020-09-10 04:25:11 +0800

    Test Initial Commit

---
+++ MouseJiggler/Utils/Constants.cs
@@ -0,0 +1,26 @@
+﻿namespace MouseJiggler.Utils
+{
+    static class Constants
+    {
+        // DLL Imports
+        public const string USER32 = "user32.dll";
+        public const string KERNEL32 = "kernel32.dll";
+
+        // Keyboard Mod
+        public const int MOD_ALT = 0x0001;
+        public const int MOD_CONTROL = 0x0002;
+        public const int MOD_NOREPEAT = 0x4000;
+        public const int MOD_SHIFT = 0x0004;
+        public const int MOD_WIN = 0x0008;
+        public const int WM_HOTKEY = 0x0312;
+
+        // Windows Commands
+        public const int WM_SYSCOMMAND = 0x0112;
+        public const int SC_MONITOR_POWER = 0xF170;
+        public const int SC_SCREENSAVE = 0xF140;
+
+        // Donnation
+        private const string donnation_link = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6JAKSVTCGHKCW&source=url";
+        public const string PAYPAL_URL = donnation_link;
+    }
+}
